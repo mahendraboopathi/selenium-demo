82dac2b7e62c3c6393907e72fb67b2719fbbd163
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-08-24 11:05:53 +0530

    Update TC_005.java

--- src/testcases/TC_005.java
+++ src/testcases/TC_005.java
@@ -8,7 +8,7 @@ public class TC_005 {
 	@Test
 	public void testcase1()
 	{
-		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver","C:/Users/New/Desktop/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
