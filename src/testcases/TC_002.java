41650dc6fb835f3a3645ad4b732fb6de2a07485e
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-09-20 19:15:36 +0530

    Update TC_002.java

--- src/testcases/TC_002.java
+++ src/testcases/TC_002.java
@@ -8,7 +8,7 @@ public class TC_002 {
 	@Test
 	public void testcase1()
 	{
-		System.setProperty("WebDriver.Chrome.driver", "E://chromedriver");
+		System.setProperty("WebDriver.Chrome.driver", ""C:/Users/New/Desktop/chromedriver.exe"");
 		ChromeDriver driver= new ChromeDriver();
 		driver.get("http://54.144.210.255:8081/");
 		driver.findElementById("inputEmail3").sendKeys("info@roboapp.com");
