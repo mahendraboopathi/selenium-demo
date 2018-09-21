5dc0356060694aaa917da08ab50e8a907b1db4a2
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-09-21 15:32:43 +0530

    Update TC_002.java

--- src/testcases/TC_002.java
+++ src/testcases/TC_002.java
@@ -9,10 +9,10 @@ public class TC_002 {
 	public void testcase1()
 	{
 		System.setProperty("WebDriver.Chrome.driver", "C:/Users/New/Desktop/chromedriver.exe");
-		ChromeDriver driver= new ChromeDriver();
+		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://54.144.210.255:8081/");
 		driver.findElementById("inputEmail3").sendKeys("info@roboapp.com");
-		driver.findElementById("inputPassword3").sendKeys("hello");
+		driver.findElementById("inputPassword3").sendKeys("Super123");
 		driver.findElementByXpath("//*[@id="loginForm"]/div[3]/div/button").click();
 	}
 	
