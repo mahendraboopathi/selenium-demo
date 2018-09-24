43ed3a5e20645edebe612aedeb28341f66fb6bd5
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-09-24 18:15:49 +0530

    Update TC_002.java

--- src/testcases/TC_002.java
+++ src/testcases/TC_002.java
@@ -13,7 +13,7 @@ public class TC_002 {
 		driver.get("http://54.144.210.255:8081/");
 		driver.findElementById("inputEmail3").sendKeys("info@roboapp.com");
 		driver.findElementById("inputPassword3").sendKeys("Super123");
-		driver.findElement(By.xpath("//button[@type='submit']")).submit();
+// 		driver.findElement(By.xpath("//button[@type='submit']")).submit();
 // 		driver.findElement(By.xpath("//*[@id="loginForm"]/div[3]/div/button")).click();
 // 		driver.findElementByXpath("//*[@id="loginForm"]/div[3]/div/button").click();
 	       
