4887ad667e1cc47638db5fc6e51e3873faa17512
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-09-24 13:15:01 +0530

    Update TC_003.java

--- src/testcases/TC_003.java
+++ src/testcases/TC_003.java
@@ -11,7 +11,6 @@ public class TC_003 {
 		System.setProperty("webdriver.chrome.driver","C:/Users/New/Desktop/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://54.144.210.255:8081/");
-		driver.manage().timeout().inplicitlywait(3,timeunit.seconds);
 		driver.findElement(By.cssselector("#inputEmail3")).sendKeys("info@roboapp.com");
 		driver.findElement(By.cssseletor("#inputPassword3")).sendKeys("Super123");
 		driver.findElement(By.cssseletor("#loginForm > div:nth-child(3) > div > button")).click();
