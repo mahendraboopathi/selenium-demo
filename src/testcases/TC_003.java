2f55def1f264862a2cda2e15612e5fa994aa7579
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-09-24 11:49:41 +0530

    Update TC_003.java

--- src/testcases/TC_003.java
+++ src/testcases/TC_003.java
@@ -10,9 +10,13 @@ public class TC_003 {
 	{
 		System.setProperty("webdriver.chrome.driver","C:/Users/New/Desktop/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.facebook.com");
-		driver.findElementById("email").sendKeys("Hello");
-		driver.findElementById("pass").sendKeys("Hello");
+		driver.get("http://54.144.210.255:8081/");
+		driver.findElementById("inputEmail3").sendKeys("info@roboapp.com");
+		driver.findElementById("inputPassword3").sendKeys("Super123");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id="loginForm"]/div[3]/div/button")).click();
+		Thread.sleep(5000);
+	
 		driver.quit();
     
 	}
