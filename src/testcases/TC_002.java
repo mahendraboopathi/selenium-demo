e20dd27636e63f6c0ae81f8bd81455073b57a1cf
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-09-24 18:50:56 +0530

    Update TC_002.java

--- src/testcases/TC_002.java
+++ src/testcases/TC_002.java
@@ -10,12 +10,18 @@ public class TC_002 {
 	{
 		System.setProperty("WebDriver.Chrome.driver", "C:/Users/New/Desktop/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://54.144.210.255:8081/");
-		driver.findElementById("inputEmail3").sendKeys("info@roboapp.com");
-		driver.findElementById("inputPassword3").sendKeys("Super123");
-// 		driver.findElement(By.xpath("//button[@type='submit']")).submit();
-// 		driver.findElement(By.xpath("//*[@id="loginForm"]/div[3]/div/button")).click();
-// 		driver.findElementByXpath("//*[@id="loginForm"]/div[3]/div/button").click();
+		driver.get("http://54.144.210.255:8081");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("username")).sendKeys("info@roboapp.com");
+		 driver.findElement(By.name("password")).sendKeys("Super123");
+		 driver.findElement(By.xpath("//button[@type='submit']")).click();
+		 try {
+			Thread.sleep(8000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 driver.findElement(By.xpath("//button[@type='submit']")).submit();
 	       
 	
 	}
