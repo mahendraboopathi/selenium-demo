541712fd6eaa6bc4955f2c02e8b5044e177c1682
mahendraboopathi <40461777+mahendraboopathi@users.noreply.github.com>
2018-09-25 15:42:08 +0530

    Update TC_002.java

--- src/testcases/TC_002.java
+++ src/testcases/TC_002.java
@@ -38,14 +38,14 @@ public class TC_002 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-// 		 driver.findElement(By.xpath("//*[@id="view_0"]")).click();
-// 		 try {
-// 			Thread.sleep(8000);
-// 		} catch (InterruptedException e) {
-// 			// TODO Auto-generated catch block
-// 			e.printStackTrace();
-// 		}
-// 		driver.findElement(By.xpath("//*[@id="app_0"]/td[1]/div/div/div/div[2]/div/div[2]/div[4]/span/ul/li[2]/button")).click();
+ 		 driver.findElement(By.id("view_0")).click();
+ 		 try {
+ 			Thread.sleep(8000);
+ 		} catch (InterruptedException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+		}
+ 		driver.findElement(By.xpath("//button[@type='button']")).click();
                 driver.quit();		
 		 
 		
